d9a295b4cc99d3f40461e0c7794158d453924a28
ilia2003 <37469000+ilia2003@users.noreply.github.com>
2023-10-07 17:21:26 +0200

    Add files via upload

---
+++ int a=5;.cs
@@ -0,0 +1,10 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 5
+int c = a+b;
+        global::System.Object value = Console.WriteLine(c);
+    }
+}
